a848772e5e5a5d4ec17f08757d9fd48e6dac5a9e
L.Answer <answer_ljm@163.com>
2018-12-17 19:37:15 +0800

    订单|库存|仓储|积分系统基版提交

---
+++ scis-integral-service/src/main/java/com/xiwei/ScisIntegralServiceApplication.java
@@ -0,0 +1,14 @@
+package com.xiwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScisIntegralServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScisIntegralServiceApplication.class, args);
+	}
+
+}
+
---
+++ scis-warehousing-service/src/main/java/com/xiwei/ScisWarehousingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.xiwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScisWarehousingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScisWarehousingServiceApplication.class, args);
+	}
+
+}
+

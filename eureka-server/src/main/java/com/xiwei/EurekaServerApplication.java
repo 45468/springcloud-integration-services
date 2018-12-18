874f4e8ea223bcfbe965a205feb236d2dde5611d
L.Answer <answer_ljm@163.com>
2018-12-18 11:35:11 +0800

    Eureka Server

--- eureka-server/src/main/java/com/xiwei/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/xiwei/EurekaServerApplication.java
@@ -2,8 +2,11 @@ package com.xiwei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+/* 启动一个服务注册中心 */
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
